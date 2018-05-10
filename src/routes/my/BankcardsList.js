de980a976bef9ffd5ed21db878303a908220fc52
youngi <youngiyang@163.com>
2018-05-10 22:22:12 +0800

    完成Bankcard组件

---
+++ src/routes/my/BankcardsList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bankcard from '../../components/bankcard/'
+class BankcardsList extends React.Component{
+  render (){
+    return(
+      <div>
+        <Bankcard />
+      </div>
+    )
+  }
+}
+
+export default BankcardsList
