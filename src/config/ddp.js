fcc7febf8d22bed0d741d1513eb49addc9e45139
youngiyang <youngiyang@localhost.localdomain>
2018-07-10 02:03:37 +0800

    修改ddp配置

--- src/config/ddp.js
+++ src/config/ddp.js
@@ -1,8 +1,8 @@
 import DDP from "ddp.js";
 const options = {
-    endpoint: "ws:/127.0.0.1:5000/websocket",
+    // endpoint: "ws:/127.0.0.1:5000/websocket",
     // endpoint: "ws:/p.10000cars.cn:8085/websocket",//for test
-    // endpoint: "ws:/p.10000cars.cn/websocket",//for production
+    endpoint: "ws:/p.10000cars.cn/websocket",//for production
     SocketConstructor:  WebSocket
     
 };
