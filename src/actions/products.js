f1ca274b2bda6ec4221d8611707d0329fa6f8ae7
= <shindouhiro@gmail.com>
2018-08-09 13:52:50 +0800

    修改代理接口

--- src/actions/products.js
+++ src/actions/products.js
@@ -170,27 +170,27 @@ export function getShopProductsLimit(shopId,page){
   const appName = app.name;
   console.log(appName);
     return (dispatch, getState) => {
-        dispatch(expectGetShopProductsLimit());
-        return axios.get(`${serverConfig.server_url}/api/new_add_products`,{
-          params:{
-            appName
-          }
-        }).then((res)=>{
-            console.log(res)
-            dispatch(getShopProductsLimitSuccess(res.data.products))
-        }).catch((err)=>{
-            console.log(err)
-            dispatch(getShopProductsLimitFail())
-        })
+        // dispatch(expectGetShopProductsLimit());
+        // return axios.get(`${serverConfig.server_url}/api/new_add_products`,{
+        //   params:{
+        //     appName
+        //   }
+        // }).then((res)=>{
+        //     console.log(res)
+        //     dispatch(getShopProductsLimitSuccess(res.data.products))
+        // }).catch((err)=>{
+        //     console.log(err)
+        //     dispatch(getShopProductsLimitFail())
+        // })
 
 
         // 
-        // return getRemoteMeteor(
-        //     dispatch,getState, "products",
-        //     "app.get.products.shop.limit",
-        //     [],
-        //     getShopProductsLimitSuccess, getShopProductsLimitFail
-        // );
+        return getRemoteMeteor(
+            dispatch,getState, "products",
+            "app.get.products.shop.limit",
+            [],
+            getShopProductsLimitSuccess, getShopProductsLimitFail
+        );
     }
 }
 
