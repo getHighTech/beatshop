d04ed21a34ea25ec3a86f56f2cd80352123f5b3d
= <shindouhiro@gmail.com>
2018-09-03 18:05:39 +0800

    部署

--- src/actions/token.js
+++ src/actions/token.js
@@ -1,6 +1,7 @@
 
 import Axios from 'axios';
 import { setStore, getStore } from '../tools/localStorage';
+import   serverConfig   from '../config/server'
 
 const uuidv4 = require('uuid/v4');
 
@@ -15,7 +16,7 @@ export async function getToken(){
     }
   }else{
     let uuid = uuidv4();
-    let rlt = await Axios.get("http://test2.10000cars.cn/api/v1/get_token?uuid="+uuid)
+    let rlt = await Axios.get(`${serverConfig.server_url}/api/v1/get_token?uuid=${uuid}`)
     let token = rlt.data.msgCiphered;
     await setStore("token", token);
     await setStore("uuid", uuid);
