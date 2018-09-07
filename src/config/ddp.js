76867b6f77f50b0457030f658b5897dc363ab6e6
= <shindouhiro@gmail.com>
2018-09-07 16:34:55 +0800

    部署

--- src/config/ddp.js
+++ src/config/ddp.js
@@ -4,9 +4,9 @@ const options = {
 //
     // endpoint: "ws:/192.168.31.254:5000/websocket",
     // endpoint: "ws:/p.10000cars.cn:8085/websocket",//for test
-    // endpoint: "ws:/p.10000cars.cn/websocket",//for production
+    endpoint: "ws:/p.10000cars.cn/websocket",//for production
     //  endpoint: "ws:/10.90.105.182:5000/websocket",
-     endpoint: "ws:/192.168.31.162:5000/websocket",
+    //  endpoint: "ws:/192.168.31.162:5000/websocket",
 
 
      SocketConstructor:  WebSocket
