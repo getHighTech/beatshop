1e52a17431dcdd15956ba69cdadeb918e8a6dc4e
= <shindouhiro@gmail.com>
2018-09-07 16:41:19 +0800

    拉不了新代码

--- src/config/ddp.js
+++ src/config/ddp.js
@@ -6,7 +6,6 @@ const options = {
     // endpoint: "ws:/p.10000cars.cn:8085/websocket",//for test
     endpoint: "ws:/p.10000cars.cn/websocket",//for production
     //  endpoint: "ws:/10.90.105.182:5000/websocket",
-    //  endpoint: "ws:/192.168.31.162:5000/websocket",
 
 
      SocketConstructor:  WebSocket
