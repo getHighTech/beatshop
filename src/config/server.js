e440b1b142e2acd6fe8d2cbaa774fc7a369d7289
= <shindouhiro@gmail.com>
2018-09-11 12:50:57 +0800

    修改订单刷新订单丢失

--- src/config/server.js
+++ src/config/server.js
@@ -1,6 +1,6 @@
 const serverConfig = {
-    server_url: "http://xianzhi.10000cars.cn"
-    // server_url: "http://192.168.31.162:1235"
+    // server_url: "http://xianzhi.10000cars.cn"
+    server_url: "http://192.168.31.254:1235"
     // server_url: "http://localhost:1235"
 };
 
