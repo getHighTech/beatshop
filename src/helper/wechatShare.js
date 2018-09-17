b402b9b70b7e810f52b226f97b468e1b94c81896
Your Name <you@example.com>
2018-09-17 16:23:29 +0800

    修改微信分享id

--- src/helper/wechatShare.js
+++ src/helper/wechatShare.js
@@ -21,7 +21,7 @@ export const wechatShare =(product)=>{
     console.log(result);
     wx.config({
           debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: 'wx412cc1c5e02a292e',
+          appId: 'wx0564668ed5671740',
           timestamp: result.timestamp, // 必填，生成签名的时间戳
           nonceStr: result.nonceStr, // 必填，生成签名的随机串
           signature: result.signature,// 必填，签名
